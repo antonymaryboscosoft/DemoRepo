153bae7b6719cfb38aaed9f6b98c0bcf2043eca1
pushparanjitha <a.pushparanjitha@gmail.com>
2018-05-15 12:34:17 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class LoginPage {
+	public void lognPage(){
+		System.out.print("loginpage test");
+	}
+
+}
---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,8 @@
+package com.qa.test;
+
+public class DemoTest {
+	public void demoTest(){
+		System.out.print("demoTest method");
+	}
+
+}
