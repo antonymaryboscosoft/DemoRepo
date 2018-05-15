81d1d62dc77a365dbfa37a9d8a8711b77266f816
pushparanjitha <a.pushparanjitha@gmail.com>
2018-05-15 14:42:24 +0530

    add search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -3,6 +3,9 @@ package com.qa.pages;
 public class LoginPage {
 	public void lognPage(){
 		System.out.print("loginpage test");
+		
 	}
-
+public void loginset(){
+	System.out.print("loginpage set");	
+}
 }
